5571fc5f17895df6d606c66b4fc3daf794751d46
= <=>
2025-07-28 22:03:12 +0700

    add fungsi di js untuk menampilkan data di local strorage

---
+++ profile-users.js
@@ -0,0 +1,45 @@
+// mengambil data local storage bagian password dan username
+let put_data_username_password = localStorage.getItem("my-key-value");
+let convert_json_to_obj_username_password = JSON.parse(
+  put_data_username_password
+);
+
+// mengambil data local storage bagian addres dan date
+let put_data_local_addres_date = localStorage.getItem("addres-date");
+let convert_json_to_obj_addres_date = JSON.parse(put_data_local_addres_date);
+
+// fungsi untuk mengambil data dari local storage yaitu username dari user yang telah register tadi
+const put_username = function (username_users) {
+  for (let key in username_users) {
+    let put_profile_username = document.querySelector("#profile-username");
+    let create_p_for_profile_username = document.createElement("p");
+    create_p_for_profile_username.innerHTML = key;
+    put_profile_username.appendChild(create_p_for_profile_username);
+  }
+  return username_users;
+};
+put_username(convert_json_to_obj_username_password);
+
+// fungsi untuk mengambil data dari local storage yaitu alamat dari user yang telah register tadi
+const put_addres = function (addres_users) {
+  for (let key in addres_users) {
+    let put_profile_addres = document.querySelector("#profile-addres");
+    let create_p_for_profile_addres = document.createElement("p");
+    create_p_for_profile_addres.innerHTML = key;
+    put_profile_addres.appendChild(create_p_for_profile_addres);
+  }
+  return addres_users;
+};
+put_addres(convert_json_to_obj_addres_date);
+
+// fungsi untuk mengambil data dari local storage yaitu tanggal lahir dari user yang telah register tadi
+const put_date = function (date_users) {
+  for (let key in date_users) {
+    let put_profile_date = document.querySelector("#profile-date");
+    let create_p_for_profile_date = document.createElement("p");
+    create_p_for_profile_date.innerHTML = date_users[key];
+    put_profile_date.appendChild(create_p_for_profile_date);
+  }
+  return date_users;
+};
+put_date(convert_json_to_obj_addres_date);
--- register.js
+++ register.js
@@ -4,6 +4,7 @@ let register_us = document.querySelector("#us-register");
 let register_pw = document.querySelector("#pw-register");
 let register_addres = document.querySelector("#addres-register");
 let register_date = document.querySelector("#date-register");
+let register_img = document.querySelector("#profile-register");
 
 // id dari form registrasi
 let form_regis = document.querySelector("#form-regis");
@@ -15,6 +16,7 @@ let btn_reset_regis = document.querySelector("#reset-regis");
 // obj kosong untuk menyimpan data ke local-storage
 let username_password = {};
 let addres_date = {};
+let img_users = {};
 
 // event saat register
 form_regis.addEventListener("submit", function (e) {
@@ -23,11 +25,14 @@ form_regis.addEventListener("submit", function (e) {
   let value_input_register_password = register_pw.value;
   let value_input_register_addres = register_addres.value;
   let value_input_register_date = register_date.value;
+  let value_input_register_img = register_img.value;
   username_password[value_input_register_username] =
     value_input_register_password;
   addres_date[value_input_register_addres] = value_input_register_date;
+  img_users[value_input_register_img] = "none";
   localStorage.setItem("my-key-value", JSON.stringify(username_password));
   localStorage.setItem("addres-date", JSON.stringify(addres_date));
+  localStorage.setItem("img-users-profile", JSON.stringify(img_users));
   alert("Register berhasil!!!");
   window.location = "index.html";
 });
